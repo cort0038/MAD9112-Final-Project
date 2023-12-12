8c314384d01f601a0471a613cc6219e6c3966279
Luis Cortorreal <cort0038@algonquinlive.com>
2023-12-12 12:11:29 -0500

    MAD9112 Final Project Complete

--- main.js
+++
@@ -1,7 +0,0 @@
-
-function getDaysUntilChristmas() {
-  const today = new Date().getDate()
-  const christmasDay = 25
-  return christmasDay - today
-}
-console.log(`We are ${getDaysUntilChristmas()} days away from Christmas`)
